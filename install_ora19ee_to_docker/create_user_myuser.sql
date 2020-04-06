673489b5fe03752ff45b184851a0cf26dd0bd4ef
Kivilev Denis <d.kivilev@qiwi.com>
2020-04-06 11:24:40 +0300

    add files for tutorial Oracle 19EE to Docker

---
+++ install_ora19ee_to_docker/create_user_myuser.sql
@@ -0,0 +1,14 @@
+-- ******** Создание пользователя MYUSER **********
+
+-- drop user myuser cascade;
+
+create user myuser
+identified by booble34
+default tablespace users
+temporary tablespace temp
+profile default
+quota unlimited on users;
+  
+grant resource to myuser;
+grant connect to myuser;
+grant debug connect session to myuser;
