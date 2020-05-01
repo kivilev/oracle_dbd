73437cadc95aba41098c62d3191955fbdd881129
Kivilev Denis <d.kivilev@qiwi.com>
2020-05-01 16:20:43 +0300

    Add Copyright

--- dbms_lock/lockdemo/src/main/java/ru/kivilev/OracleLockDemoApp.java
+++ dbms_lock/lockdemo/src/main/java/ru/kivilev/OracleLockDemoApp.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2020. Пример использования именованных блокировок на уровне JavaApp.
+ * Автор: Кивилев Денис
+ *
+ * Репозиторий: https://github.com/kivilev/oracle_dbd
+ * Телеграм - https://t.me/oracle_dbd
+ * Ютуб - https://www.youtube.com/channel/UC4f5yrDUHl0R_twMttfL7XA
+ */
+
 package ru.kivilev;
 
 import org.slf4j.Logger;
